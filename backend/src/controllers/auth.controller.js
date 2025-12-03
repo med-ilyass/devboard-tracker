6b27e652a5000356cd869ecd2fdda5e0aa101b6a
ilyass <ilyass.oudli@gmail.com>
2025-12-02 22:50:05 -0500

    adding auth controller and auth.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,13 +1,50 @@
-//POST api/auth/register
-export function register(req, res) {
-    res.json({ message: "regiter route works" })
-}
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import pool from "../config/db.js"
 
+const SALT_ROUND = 10;
+function generateToken(user) {
+    return jwt.sign({
+        id: user.id,
+        email: user.email,
+        role: user.role
+    },
+        process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXOIRES_IN || "7d"
+    })
+}
+//POST api/auth/register
+export async function register(req, res) {
+    //res.json({ message: "regiter route works" })
+    try {
+        const { email, password, username, role } = req.body;
+        if (!email || !password || !username) {
+            return res.status(400).json({ message: "Eamil, password and name are required!" })
+        }
+        //let's check if user exists
+        const existing = await pool.query(`select id from users where Email = $1`, [email])
+        if (existing.rows.length > 0) {
+            return res.status(409).json({ message: "Email already in use!" })
+        }
+        //let's hash the password
+        const passwordHash = await bcrypt.hash(password, SALT_ROUND);
+        const userRole = role || "devoloper"
+        const insertQuery = `Insert into users (email, password_hash, name, role) values ($1, $2, $3, $4) RETURNING id, email, username, role, created_at`;
+        const result = await pool.query(insertQuery, [email, passwordHash, username, userRole]);
+        const user = result.rows[0];
+        const token = generateToken(user);
+        return res.status(201).json({
+            user, token
+        })
+    } catch (error) {
+        console.error("Error in register!", error.message)
+        return res.status(500).json({ error: "Server error during registration!" })
+    }
+}
 //POST api/auth/login
 export function login(req, res) {
     res.json({ message: "login route works" })
 }
-
 //GET api/auth/me
 export function getMe(req, res) {
     res.json({ message: "me route works" })
--- backend/src/controllers/tasks.controller.js
+++ backend/src/controllers/tasks.controller.js
@@ -1,18 +1,15 @@
 import pool from "../config/db.js";
-
 //get all tasks
 export async function getTasks(req, res) {
     // res.json({ message: "Get all task works!" })
     try {
         const { projectId } = req.query;
-
         let result;
         if (projectId) {
             const id = Number(projectId)
             if (Number.isNaN(id)) {
                 return res.status(404).json({ message: "Invalid project id in query" })
             }
-
             result = await pool.query(`
                 SELECT id, project_id, title, description, status, priority,
                created_by, assigned_to, due_date, created_at, updated_at
@@ -36,14 +33,12 @@ export async function getTasks(req, res) {
         res.status(500).json({ err: "Failing fetshing tasks!" })
     }
 }
-
 //get tasks by id
 export async function getTaskById(req, res) {
     // res.json({ message: "Get task by is works" })
     try {
         const { taskId } = req.parms;
         const id = Number(taskId);
-
         if (Number.isNaN(id)) {
             return res.status(400).json({ message: "Invalid task id" })
         }
@@ -63,7 +58,6 @@ export async function getTaskById(req, res) {
         res.status(500).json({ error: "Failing fetshing Task by ID!" })
     }
 }
-
 //create task
 // POST /api/tasks
 // Body: { project_id, title, description?, status?, priority?, created_by, assigned_to?, due_date? }
@@ -91,11 +85,9 @@ export async function createTask(req, res) {
         if (Number.isNaN(projectId) || Number.isNaN(createdBy) || (assigned_to && Number.isNaN(assignedTo))) {
             return res.status(400).json({ message: "Invalid numeric field(s)" })
         }
-
         const insertQuery = `INSERT INTO tasks (project_id, title, description, status, priority,
         created_by, assigned_to, due_date) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)  RETURNING id, project_id, title, description, status, priority,
                 created_by, assigned_to, due_date, created_at, updated_at`;
-
         const result = await pool.query(insertQuery, [projectId,
             title,
             description || null,
@@ -104,15 +96,11 @@ export async function createTask(req, res) {
             createdBy,
             assignedTo,
             due_date || null]);
-
         const task = result.rows[0];
         res.status(201).json(task)
-
-
     } catch (error) {
         console.error("Error in create task!1", error)
         res.status(500).json({ error: "Failed to create task." })
-
     }
 }
 // PATCH /api/tasks/:taskId
@@ -121,18 +109,14 @@ export async function updateTask(req, res) {
     try {
         const { taskId } = req.params;
         const id = Number(taskId);
-
         if (Number.isNaN(id)) {
             return res.status(400).json({ message: "Invalid task id" });
         }
-
         const { title, description, status, priority, assigned_to, due_date } = req.body;
-
         const assignedTo = assigned_to ? Number(assigned_to) : null;
         if (assigned_to && Number.isNaN(assignedTo)) {
             return res.status(400).json({ message: "Invalid assigned_to" });
         }
-
         const updateQuery = `
       UPDATE tasks
       SET
@@ -146,7 +130,6 @@ export async function updateTask(req, res) {
       RETURNING id, project_id, title, description, status, priority,
                 created_by, assigned_to, due_date, created_at, updated_at
     `;
-
         const result = await pool.query(updateQuery, [
             title ?? null,
             description ?? null,
@@ -156,11 +139,9 @@ export async function updateTask(req, res) {
             due_date ?? null,
             id
         ]);
-
         if (result.rows.length === 0) {
             return res.status(404).json({ message: "Task not found" });
         }
-
         const task = result.rows[0];
         res.json(task);
     } catch (error) {
@@ -168,7 +149,6 @@ export async function updateTask(req, res) {
         res.status(500).json({ message: "Failed to update task" });
     }
 }
-
 // DELETE /api/tasks/:taskId
 export async function deleteTask(req, res) {
     try {
@@ -178,7 +158,6 @@ export async function deleteTask(req, res) {
         if (Number.isNaN(id)) {
             return res.status(400).json({ message: "Invalid task id" });
         }
-
         const result = await pool.query(
             `
       DELETE FROM tasks
@@ -187,12 +166,10 @@ export async function deleteTask(req, res) {
                 created_by, assigned_to, due_date, created_at, updated_at
       `,
             [id]
-        );
-
+        )
         if (result.rows.length === 0) {
             return res.status(404).json({ message: "Task not found" });
         }
-
         const task = result.rows[0];
         res.json({ message: "Task deleted", task });
     } catch (error) {
