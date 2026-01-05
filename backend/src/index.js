4c146c5384df588e814ec3b2e6b7315be6cb48bb
ilyass <ilyass.oudli@gmail.com>
2026-01-04 21:22:36 -0500

    feat: add contact page and integrate contact form with backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,9 @@ import cors from "cors"
 import authRoutes from "./routes/auth.routes.js"
 import projectsRoutes from "./routes/projects.routes.js"
 import tasksRoutes from "./routes/tasks.routes.js"
+import contactRoutes from "./routes/contact.routes.js";
+
+
 
 const app = express();
 const PORT = 4000;
@@ -18,6 +21,7 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes)
 app.use("/api/projects", projectsRoutes)
 app.use("/api/tasks", tasksRoutes)
+app.use("/api/contact", contactRoutes);
 
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`)
