4083f341c8f0e1ab6ee9a53c1d0f349071e41482
ilyass <ilyass.oudli@gmail.com>
2026-01-07 00:01:59 -0500

    removing snowflack lib

--- frontend/devboard/src/pages/ForgotPassword.jsx
+++ frontend/devboard/src/pages/ForgotPassword.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom"
-import Snowfall from "react-snowfall"
 export default function ForgotPassword() {
 
     const [error, setError] = useState("");
