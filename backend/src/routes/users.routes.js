0a7f56c9f4264f8f7cf2fdd87b91b7e225b7ee89
ilyass <ilyass.oudli@gmail.com>
2026-01-09 23:58:47 -0500

    fixing backeend issue {}

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express"
 import authMiddleware from "../middleware/auth.js";
-import searchUsers from "../controllers/users.controller.js"
+import {searchUsers} from "../controllers/users.controller.js"
 const router = Router();
 
 router.use(authMiddleware);
